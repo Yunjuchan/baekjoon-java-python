a6cc6d9924e426227857519aaa294ab4a5091d4c
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2024-04-23 00:17:52 +0900

    [Silver IV] Title: 문자열 집합, Time: 280 ms, Memory: 116168 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+s = set()
+for _ in range(N) :
+    s.add(input().rstrip())
+
+cnt = 0
+for _ in range(M) :
+    if input().rstrip() in s :
+        cnt += 1
+print(cnt)
