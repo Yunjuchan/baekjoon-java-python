b61715c4753c6a8df52c5f7abb4077504d9577c8
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2024-09-01 20:05:38 +0900

    [Gold III] Title: 회식구호, Time: 564 ms, Memory: 48804 KB -BaekjoonHub

--- 백준/Gold/16158. 회식구호/회식구호.java
+++ 백준/Gold/16158. 회식구호/회식구호.java
@@ -1,17 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
     public static class Voice {
-        BigInteger A;
-        BigInteger B;
+        long A;
+        long B;
         int n;
 
-        public Voice(BigInteger A, BigInteger B, int n) {
+        public Voice (long A, long B, int n) {
             this.A = A;
             this.B = B;
             this.n = n;
@@ -29,11 +28,11 @@ public class Main {
         String result = "-1";
         int cnt = 0;
 
-        for (int i = 0; i < N; i++) {
-            int v = Integer.parseInt(st.nextToken());
-            BigInteger A = BigInteger.valueOf(v).multiply(BigInteger.valueOf(X));
-            BigInteger B = BigInteger.valueOf(100);
-            double key = A.doubleValue() / B.doubleValue();
+        for (int i=0; i<N; i++) {
+            long v = Long.parseLong(st.nextToken());
+            long A = v * X;
+            long B = 100;
+            double key = (double) A / (double) B;
 
             if (M.containsKey(key)) {
                 Voice voice = M.get(key);
@@ -42,29 +41,21 @@ public class Main {
                 M.put(key, new Voice(A, B, 1));
             }
 
-            // A와 B 계산 로직
-            A = BigInteger.valueOf(v).multiply(BigInteger.valueOf(2000000 - X * 10000));
-            B = BigInteger.valueOf(1000000 - 1);
-//            System.out.println(A + " " + B);
+            A = v * (2000000 - X * 10000);
+            B = 1000000-1;
 
-            key = A.doubleValue() / B.doubleValue();
-//            System.out.println("key" + key);
-
+            key = (double) A / (double) B;
             if (M.containsKey(key)) {
                 Voice voice = M.get(key);
-                M.replace(key, new Voice(voice.A, voice.B, voice.n - 1));
+                M.replace(key, new Voice(voice.A, voice.B, voice.n-1));
             } else {
                 M.put(key, new Voice(A, B, -1));
             }
         }
-
-        BigInteger resultA = BigInteger.ZERO;
-        BigInteger resultB = BigInteger.ZERO;
+        long resultA = 0;
+        long resultB = 0;
         for (Voice voice : M.values()) {
-
-//            System.out.println(voice.A + " " + voice.B + " " + voice.n);
             cnt += voice.n;
-//            System.out.println(cnt);
             if (cnt >= K) {
                 resultA = voice.A;
                 resultB = voice.B;
@@ -72,24 +63,28 @@ public class Main {
             }
         }
 
-        if (!resultA.equals(BigInteger.ZERO) && !resultB.equals(BigInteger.ZERO)) {
-            BigInteger g = gcd(resultA, resultB);
-            resultA = resultA.divide(g);
-            resultB = resultB.divide(g);
-            if (resultB.equals(BigInteger.ONE)) {
-                result = resultA.toString();
+        if (resultA != 0 && resultB != 0) {
+            long g = gcd(resultA, resultB);
+            resultA /= g;
+            resultB /= g;
+            if (resultB == 1) {
+                result = Long.toString(resultA);
             } else {
-                result = resultA.toString() + "/" + resultB.toString();
+                result = Long.toString(resultA) + "/" + Long.toString(resultB);;
             }
         }
         System.out.println(result);
     }
-
-    public static BigInteger gcd(BigInteger A, BigInteger B) {
-        while (!B.equals(BigInteger.ZERO)) {
-            BigInteger temp = B;
-            B = A.mod(B);
-            A = temp;
+    public static long gcd(long A, long B) {
+        if (A < B) {
+            long tmp = A;
+            A = B;
+            B = tmp;
+        }
+        while (B !=0) {
+            long tmp = B;
+            B = A % B;
+            A = tmp;
         }
         return A;
     }
