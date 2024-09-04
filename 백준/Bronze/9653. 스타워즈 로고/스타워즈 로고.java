813297179dd60e153ad4a996701c7141a652eadc
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2024-09-04 23:01:12 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 84 ms, Memory: 13792 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,21 @@
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("    8888888888  888    88888\n")
+                .append("   88     88   88 88   88  88\n")
+                .append("    8888  88  88   88  88888\n")
+                .append("       88 88 888888888 88   88\n")
+                .append("88888888  88 88     88 88    888888\n")
+                .append("\n")
+                .append("88  88  88   888    88888    888888\n")
+                .append("88  88  88  88 88   88  88  88\n")
+                .append("88 8888 88 88   88  88888    8888\n")
+                .append(" 888  888 888888888 88  88      88\n")
+                .append("  88  88  88     88 88   88888888\n");
+
+        System.out.println(sb.toString());
+    }
+}
