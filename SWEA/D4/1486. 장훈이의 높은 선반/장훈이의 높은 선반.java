a351821d0b140c7ed3618b6ec4851dd1dfd2888c
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2024-11-22 15:44:08 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 111 ms, Memory: 18,700 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    
+    static int result = 0;
+    static int[] H;
+    
+    public static void dfs(int level, int N, int B, int total) {
+     	if (level == N) {
+            if (total >= B) {
+            	
+                result = Math.min(result, total-B);
+            }
+            return;
+        }
+        dfs(level+1, N, B, total+H[level]);
+        dfs(level+1, N, B, total);
+    }
+    
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int T = Integer.parseInt(st.nextToken());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            result = Integer.MAX_VALUE;
+            
+            H = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i=0; i<N; i++) {
+                H[i] = Integer.parseInt(st.nextToken());
+            }
+            
+            dfs(0, N, B, 0);
+			System.out.println("#" + tc + " " + result);
+		}
+	}
+}
