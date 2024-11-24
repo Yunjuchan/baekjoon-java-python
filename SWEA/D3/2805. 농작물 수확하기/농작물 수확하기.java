0a795eb50cdff8de4c603067a1fb27aceadadd33
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2024-11-24 18:51:26 +0900

    [D3] Title: 농작물 수확하기, Time: 116 ms, Memory: 21,452 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,39 +1,29 @@
-import java.util.*;
 import java.io.*;
 
 class Solution {
-	public static void main(String args[]) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-		
-		for(int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine().trim());
             int[][] arr = new int[N][N];
-			for (int i=0; i<N; i++) {
+            for (int i = 0; i < N; i++) {
                 String line = br.readLine();
-                for (int j=0; j<N; j++) {
+                for (int j = 0; j < N; j++) {
                     arr[i][j] = line.charAt(j) - '0';
                 }
             }
+
             int result = 0;
-            
-            for (int i=0; i<N; i++) {
-                int start, end;
-                if (i <= N/2) {
-                    start = N/2 - i;
-                    end = N/2 + i;
-                } else {
-                    start = N/2 - (N - 1 - i);
-                    end = N/2 + (N - 1 - i);
-                }
-                for (int j=start; j<=end; j++) {
+            int mid = N / 2;
+            for (int i = 0; i < N; i++) {
+                int offset = Math.abs(mid - i);
+                for (int j = offset; j < N - offset; j++) {
                     result += arr[i][j];
                 }
             }
             System.out.println("#" + tc + " " + result);
-		}
-	}
+        }
+    }
 }
