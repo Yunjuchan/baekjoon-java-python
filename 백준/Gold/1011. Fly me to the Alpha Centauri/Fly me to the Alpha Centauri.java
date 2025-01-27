84687ca2c65bd0adf573202d09fd3236a888337b
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-01-27 10:08:39 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 108 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int t=0; t<T; t++) {
+            st = new StringTokenizer(br.readLine());
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+            long diff = y - x;
+
+            long n = 0;
+            long s = 0;
+            long e = diff;
+            while (s <= e) {
+                long mid = (s + e) / 2;
+                if (mid * (mid+1) < diff) {
+                    s = mid + 1;
+                } else {
+                    n = mid;
+                    e = mid - 1;
+                }
+            }
+
+            long cnt = 2 * n;
+            if (diff - n * (n-1) <= n) {
+                cnt--;
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
