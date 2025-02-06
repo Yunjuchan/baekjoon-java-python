0d578816886519bc48f56622140094c82de405d2
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-02-06 17:24:27 +0900

    [Silver IV] Title: Kieszonkowe, Time: 380 ms, Memory: 76180 KB -BaekjoonHub

--- 백준/Silver/17756. Kieszonkowe/Kieszonkowe.java
+++ 백준/Silver/17756. Kieszonkowe/Kieszonkowe.java
@@ -7,17 +7,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         int sum = 0;
         int min = Integer.MAX_VALUE;
         boolean hasOdd = false;
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            sum += arr[i];
-            if (arr[i] % 2 == 1) {
+            int x = Integer.parseInt(st.nextToken());
+            sum += x;
+            if (x % 2 == 1) {
                 hasOdd = true;
-                min = Math.min(min, arr[i]);
+                min = Math.min(min, x);
             }
         }
 
