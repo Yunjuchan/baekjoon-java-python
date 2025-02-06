22516dd418c68a7319741c322a7599f9a450b757
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-02-06 17:23:10 +0900

    [Silver IV] Title: Kieszonkowe, Time: 392 ms, Memory: 80164 KB -BaekjoonHub

--- 백준/Silver/17756. Kieszonkowe/Kieszonkowe.java
+++ 백준/Silver/17756. Kieszonkowe/Kieszonkowe.java
@@ -9,24 +9,25 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
         int sum = 0;
-        List<Integer> odds = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        boolean hasOdd = false;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             sum += arr[i];
             if (arr[i] % 2 == 1) {
-                odds.add(arr[i]);
+                hasOdd = true;
+                min = Math.min(min, arr[i]);
             }
         }
 
         if (sum % 2 == 0) {
             System.out.println(sum);
         } else {
-            if (N == 1 || odds.isEmpty()) {
+            if (N == 1 || !hasOdd) {
                 System.out.println("NIESTETY");
             } else {
-                odds.sort((a, b) -> a - b);
-                System.out.println(sum - odds.get(0));
+                System.out.println(sum - min);
             }
         }
     }
