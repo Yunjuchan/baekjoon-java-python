99b99c441a478b7121d41b61e0cdf2bbcf6144b2
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-02-14 16:20:25 +0900

    [Silver II] Title: 병사 배치하기, Time: 120 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -3,11 +3,37 @@ import java.util.*;
 
 public class Main {
 
+    /**
+     * 최장 증가하는 부분 수열 O(N * logN) 풀이
+     */
+    static int N;
+    static int[] X;
+    public static int binarySearch(int target, int e) {
+        int s = 0;
+        int x = 0;
+        
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (mid >= N) {
+                break;
+            }
+            if (X[mid] > target) {
+                s = mid + 1;
+                x = mid;
+            } else if (X[mid] <= target) {
+                e = mid - 1;
+            }
+        }
+        
+        X[x + 1] = Math.max(X[x + 1], target);
+
+        return x + 1;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         int max = 1;
         int[] arr = new int[N];
         int[] dp = new int[N];
@@ -19,12 +45,25 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
+        /**
+         * 최장 증가하는 부분 수열 O(N * N) 풀이
+         */
+//        for (int i = 1; i < N; i++) {
+//            for (int j = 0; j < i; j++) {
+//                if (arr[j] > arr[i]) {
+//                    dp[i] = Math.max(dp[i], dp[j] + 1);
+//                }
+//            }
+//            max = Math.max(max, dp[i]);
+//        }
+        /**
+         * 최자 증가하는 부분 수열 O(N * logN) 푸링
+         */
+        X = new int[N + 1];
+        X[0] = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = binarySearch(arr[i], i);
             max = Math.max(max, dp[i]);
         }
 
