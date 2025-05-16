ce27ce1469e1f78613827c2e54198d7ee527bc11
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-05-16 23:14:01 +0900

    [Gold V] Title: 유아와 곰두리차, Time: 584 ms, Memory: 83644 KB -BaekjoonHub

--- 백준/Gold/20951. 유아와 곰두리차/유아와 곰두리차.java
+++ 백준/Gold/20951. 유아와 곰두리차/유아와 곰두리차.java
@@ -1,42 +1,46 @@
 import java.io.*;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
-    static int[] directY = new int[]{1,0,-1,0};
-    static int[] directX = new int[]{0,1,0,-1};
-    static List<List<Integer>> adj;
-    static int D = (int)(Math.pow(10, 9) + 7);
+
+    static final int MOD = 1_000_000_007;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        long[][] DP = new long[N+1][8];
-        adj = new ArrayList<>();
-        for (int i=0; i<=N; i++) {
+
+        int[][] dp = new int[8][N + 1];
+        Arrays.fill(dp[0], 1);
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
             adj.add(new ArrayList<>());
-            DP[i][0] = 1;
         }
 
-        for (int i=0; i<M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            adj.get(a).add(b);
-            adj.get(b).add(a);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        for (int i=1; i<8; i++) {
-            for (int j=1; j<=N; j++) {
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 1; j <= N; j++) {
                 for (int next : adj.get(j)) {
-                    DP[j][i] += DP[next][i-1];
+                    dp[i + 1][next] += dp[i][j];
+                    dp[i + 1][next] %= MOD;
                 }
-                DP[j][i] %= D;
             }
         }
-        long result = 0;
-        for (int i=1; i<=N; i++) {
-            result += DP[i][7];
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            result += dp[7][i];
+            result %= MOD;
         }
-        System.out.println((result % D));
+        System.out.println(result);
     }
 }
