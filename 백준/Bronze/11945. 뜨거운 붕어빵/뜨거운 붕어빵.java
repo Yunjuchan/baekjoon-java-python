6d56276b80d0a536d7f9f1314a38c8ee94cbf481
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-09-25 23:15:18 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        char[][] arr = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][M - j - 1] = line.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
