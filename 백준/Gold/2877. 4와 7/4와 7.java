a73dc2e186460ddd2bce2cb8d65d632b8ee94218
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-10-10 19:50:53 +0900

    [Gold V] Title: 4와 7, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long K = Long.parseLong(br.readLine());
+
+
+        String bin = Long.toBinaryString(K + 1);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < bin.length(); i++) {
+            if (bin.charAt(i) == '0') {
+                sb.append('4');
+            } else sb.append('7');
+        }
+
+        System.out.println(sb);
+    }
+}
