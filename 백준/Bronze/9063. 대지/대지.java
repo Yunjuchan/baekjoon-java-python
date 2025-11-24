09b5bda3f9a251e8966e0f076cdfb026c790a044
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-11-25 00:04:05 +0900

    [Bronze III] Title: 대지, Time: 256 ms, Memory: 27432 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int minX = 10000;
+        int minY = 10000;
+        int maxX = -10000;
+        int maxY = -10000;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+        }
+
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
+}
