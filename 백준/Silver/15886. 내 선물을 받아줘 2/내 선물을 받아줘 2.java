bcc3bea7008ac0e4d9d42f230f76c0064bfd99ce
jujuju <102795943+Yunjuchan@users.noreply.github.com>
2025-12-29 20:29:39 +0900

    [Silver III] Title: 내 선물을 받아줘 2, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int answer = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            if (s.charAt(i) == 'E' && s.charAt(i + 1) == 'W') {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
